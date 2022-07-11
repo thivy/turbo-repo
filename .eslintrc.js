d35625e4b737d7e921dbb0fbace21bea0b476591
thivy <thivyakanth@gmail.com>
2022-07-11 14:02:28 +1000

    update package name

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
-  // This tells ESLint to load the config from the package `eslint-config-acme`
-  extends: ["acme"],
+  // This tells ESLint to load the config from the package `eslint-config-trove`
+  extends: ["trove"],
   settings: {
     next: {
       rootDir: ["apps/*/"],
